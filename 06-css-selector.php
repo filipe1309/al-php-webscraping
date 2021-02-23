4d4f804749bb0d6bbcee1b1fc62cb224d91ff454
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-02-22 23:40:39 -0300

    chore: add css selector

---
+++ 06-css-selector.php
@@ -0,0 +1,15 @@
+<?php
+
+use Symfony\Component\BrowserKit\HttpBrowser;
+use Symfony\Component\HttpClient\HttpClient;
+
+require 'vendor/autoload.php';
+
+$client = HttpClient::create();
+
+$browser = new HttpBrowser($client);
+
+$crawler = $browser->request('GET', 'https://vitormattos.github.io/poc-lineageos-cellphone-list-statics/');
+
+$text = $crawler->filter('title')->text();
+var_dump($text);
