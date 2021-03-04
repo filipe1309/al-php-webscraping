75d0eb92d2de291b626bca256ca5ac69238606a2
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-03-04 20:22:21 -0300

    chore: get attr from tags

---
+++ 09-atributos.php
@@ -0,0 +1,18 @@
+<?php
+
+use Symfony\Component\BrowserKit\HttpBrowser;
+use Symfony\Component\HttpClient\HttpClient;
+
+require 'vendor/autoload.php';
+
+$client = HttpClient::create();
+
+$browser = new HttpBrowser($client);
+
+$crawler = $browser->request('GET', 'https://vitormattos.github.io/poc-lineageos-cellphone-list-statics/');
+
+$codigos = $crawler->filter('article .img-thumbnail')->each(function ($node) {
+  return $node->attr('src');
+});
+
+print_r($codigos);
